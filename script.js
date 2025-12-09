80791de8acb3aad21c63963683307b5ede4d5c24
Senthil Raja R <senthilrajasen637@gmail.com>
2025-12-09 20:14:50 +0530

    feat: add storage keys and state variables

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 (function () {
   "use strict";
 
+    var STORAGE_KEY = "expense_tracker_data";
+  var THEME_KEY = "expense_theme";
+
   var $ = function (s, c) { return (c || document).querySelector(s); };
+  var expenses = [];
+  var editId = null;
+  var currentFilter = "all";
+  var searchQuery = "";
+  var searchTimeout = null;
 })();
